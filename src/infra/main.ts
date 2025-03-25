6df303ebc66f3cb838c11dda2554606ca1065fda
Felipe Jung <felipemattoseu@gmail.com>
2025-03-25 19:50:03 -0400

    Add redis, setup e2e tests and redis

--- src/infra/main.ts
+++ src/infra/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+// import { EnvService } from "./env/env.service";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // const envService = app.get(EnvService);
+  // const port = envService.get("PORT");
+
   await app.listen(process.env.PORT ?? 3333);
 }
+
 bootstrap();
